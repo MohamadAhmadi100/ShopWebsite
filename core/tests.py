ee273b745858d4e49639036e91921fbae80eda4a
MohamadAhmadi <mohammadahmadidezhnet@gmail.com>
2021-06-15 17:43:30 +0430

    test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def add(x, y):
+    """
+    >>> add(6, 7)
+    13
+    >>> add(-2,7)
+    5
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    >>> subtract(6, 3)
+    3
+    """
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
